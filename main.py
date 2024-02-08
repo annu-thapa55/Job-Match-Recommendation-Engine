eed95ccb6c6905c99640fbcd253b77702fcc3111
Annu Thapa <annu.thapa55@gmail.com>
2024-02-08 20:36:59 +1100

    Complete solution

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pyarrow
+import psutil
 import pandas as pd
 from src.recommendation_engine.recommendation import RecommendationEngine
 from src.file_reader.read_files import *
@@ -10,11 +12,12 @@ def main():
     path_file_jobs = 'csv_files/jobs.csv'
     path_file_jobseeker = 'csv_files/jobseekers.csv'
     obj_job_match = JobMatchRecommendationEngine(path_file_jobs,path_file_jobseeker)
-    obj_job_match.set_threshold_parallel_processing(5)
+    
+    obj_job_match.set_threshold_parallel_processing(7.0)
+    
     recommendations = obj_job_match.generate_recommendations()
     sorted_recommendation = obj_job_match.sort_recommendations(recommendations)
     print(sorted_recommendation)
-  
 
 if __name__ == "__main__":
     main()
--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -1,14 +1,96 @@
-import os
-import pandas as pd
 import unittest
+from unittest.mock import patch
 from unittest.mock import MagicMock, patch
 from src.recommendation_engine.recommendation import RecommendationEngine
 
 
-class TestRecommendationEngineClass(unittest.TestCase):
+class TestRecommendationEngine(unittest.TestCase):
+    """
+    Test suite for validating the functionalities of RecommendationEngine class.
+
+    This test suite class contains individual test functions for various functionalities of the
+    RecommendationEngine class to ensure its proper functioning under different scenarios and conditions..
+    """
+    @patch.multiple(RecommendationEngine, __abstractmethods__=set())
     def setUp(self):
-        print ("TO DO ")
+        """
+        Function for setting up the test cases.
 
+        It is called before performing each test case so as to set up necessary
+        resources for testing.
+        """
+        self.engine = RecommendationEngine()
 
-if __name__ == '__main__':
+    def test_set_threshold_parallel_processing_valid(self):
+        """
+        Function for testing setting a valid threshold for parallel processing.
+
+        It ensures that setting a valid threshold within the specified range is successful.
+        """
+        self.engine.set_threshold_parallel_processing(7.5)
+        self.assertEqual(self.engine.threshold_parallel_processing, 7.5)
+
+    def test_set_threshold_parallel_processing_invalid(self):
+        """
+        Function for testing setting an invalid threshold for parallel processing.
+
+        It ensures that setting an invalid threshold, above 10 MB or below 5 MB, 
+        raises a ValueError.
+        """
+        # Setting invalid threshold above 10 MB
+        with self.assertRaises(ValueError):
+            self.engine.set_threshold_parallel_processing(12.6)
+        # Setting invalid threshold below 5 MB
+        with self.assertRaises(ValueError):
+            self.engine.set_threshold_parallel_processing(3.5)
+
+    def test_set_threshold_parallel_processing_invalid_type(self):
+        """
+        Function for testing setting an invalid type for threshold for parallel processing.
+        
+        It ensures that setting an invalid input type, string, for the threshold 
+        raises a TypeError.
+        """
+        #Setting invalid input type i.e. string 
+        with self.assertRaises(TypeError):
+            self.engine.set_threshold_parallel_processing("invalid")
+
+    @patch("os.path.getsize")
+    def test_calculate_file_size_existing_file(self, mock_getsize):
+        """
+        Function for testing calculating the size of an existing file.
+        
+        It uses mocking to test the calculate_file_size function 
+        when provided with the path of an existing file.
+        """
+        mock_getsize.return_value = 1024
+        file_size = self.engine.calculate_file_size("test_file.csv")
+        self.assertEqual(file_size, 1024)
+
+    @patch("os.path.getsize")
+    def test_calculate_file_size_non_existing_file(self, mock_getsize):
+        """
+        Function for testing calculating the size of a non-existing file.
+
+        It uses mocking to test the calculate_file_size function 
+        when provided with the path of a non-existing file.
+        """
+        mock_getsize.side_effect = FileNotFoundError
+        with self.assertRaises(FileNotFoundError):
+            self.engine.calculate_file_size("non_existing_file.csv")
+
+    def test_calculate_matching_skills(self):
+        """
+        Function for testing calculating matching skills between required and present skills.
+        
+        It ensures the accuracy of calculate_matching_skills function to  calculate
+        matching skills count and percentage.
+        """
+        matching_count, matching_percentage = self.engine.calculate_matching_skills("Python, React, MySQL", "Python, MySQL")
+        self.assertEqual(matching_count, 2)
+        self.assertEqual(round(matching_percentage,2), 66.67)
+
+    
+
+if __name__ == "__main__":
     unittest.main()
